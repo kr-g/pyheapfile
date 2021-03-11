23d5c52f0a33f952da2048e6324c814e742eddf5
kr-g <k.r.goger@gmail.com>
2021-03-11 11:17:11 +0100

    added hexdump tool

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def find_projectname():
 projectname = find_projectname()
 file = os.path.join(projectname, "__init__.py")
 
-version = "v0.0.3a"
+version = find_version(file)
 
 setuptools.setup(
     name=projectname,
